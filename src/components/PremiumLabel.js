0ec40bb43eb383b3663c5e5034e023dbea027246
Pablo Molina <contact@pablomolina.me>
2019-12-17 16:19:58 +0100

    Change PremiumLabel background on dark theme

--- src/components/PremiumLabel.js
+++ src/components/PremiumLabel.js
@@ -1,9 +1,12 @@
 import './PremiumLabel.css';
-import React from 'react';
+import React, { useContext } from 'react';
+import ConverterContext from '../contexts/ConverterContext';
 
 export default function PremiumLabel() {
+  const { theme } = useContext(ConverterContext);
+
   return (
-    <p className="PremiumLabel">
+    <p className={`PremiumLabel PremiumLabel--${theme}`}>
       with{' '}
       <strong>
         <span aria-label="Premium user icon" role="img">
